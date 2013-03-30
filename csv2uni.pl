39c50ffa3f840428e57543bd562832434c7733f7
Audrey Tang <audreyt@audreyt.org>
2013-03-31 01:03:27 +0800

    * Add pua/, raw/ and uni/ conversion

---
+++ csv2uni.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/env perl
+use utf8;
+use strict;
+use File::Slurp qw[ read_file write_file ];
+my @map = read_file( 'x-造字.csv', binmode => ':utf8' ) ;
+shift @map;
+my (%symToUni, %symToPua);
+for (@map) {
+    chomp;
+    my ($code, $uni) = split /,/, $_;
+    $symToUni{chr hex $code} = $uni; # include IDS
+    $symToPua{chr hex $code} = $uni unless length $uni > 1; # exclude IDS
+}
+for my $file (<raw/*.csv>) {
+    my $csv = read_file( $file, binmode => ':utf8' ) ;
+    $file =~ s/raw/pua/;
+    $csv =~ s/([${\join('', keys %symToPua)}])/$symToPua{$1}/ego;
+    write_file( $file, {binmode => ':utf8'}, $csv ) ;
+    print "$file\n";
+
+    $file =~ s/pua/uni/;
+    $csv =~ s/([${\join('', keys %symToUni)}])/$symToUni{$1}/ego;
+    write_file( $file, {binmode => ':utf8'}, $csv ) ;
+    print "$file\n";
+}
