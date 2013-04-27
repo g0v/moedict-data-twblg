6da1f8ac6caf2f396a2aa3269867fa200cb61740
c9s <cornelius.howl@gmail.com>
2013-04-28 02:17:39 +0800

    Extract moedict base path to an variable for configuring.

--- mkxref.pl
+++ mkxref.pl
@@ -5,12 +5,15 @@ binmode IN, ':utf8';
 <IN>;
 my %a2t = qw( 萌 發穎 );
 my %t2a = qw( 發穎 萌 );
-die "Please checkout ../moedict-webkit/ and have /a/*.json ready" unless -d "../moedict-webkit/";
+
+my $moedict_basedir = "../moedict-webkit";
+
+die "Please checkout $moedict_basedir and have /a/*.json ready" unless -d $moedict_basedir;
 my %seen;
 while (<IN>) {
     chomp;
     my ($m, undef, $t) = split /,/, $_;
-    next unless -e "../moedict-webkit/a/$m.json";
+    next unless -e $moedict_basedir . "/a/$m.json";
     next if $seen{$m,$t}++;
     next if $t =~ /\d/;
     $a2t{$m} .= "," if $a2t{$m};
@@ -20,5 +23,6 @@ while (<IN>) {
 }
 use JSON;
 use File::Slurp 'write_file';
-write_file '../moedict-webkit/a/xref.json' => encode_json({ t => \%a2t })."\n";
-write_file '../moedict-webkit/t/xref.json' => encode_json({ a => \%t2a })."\n";
+mkdir $moedict_basedir . '/a' unless -e $moedict_basedir . '/a';
+write_file $moedict_basedir . '/a/xref.json' => encode_json({ t => \%a2t })."\n";
+write_file $moedict_basedir . '/t/xref.json' => encode_json({ a => \%t2a })."\n";
